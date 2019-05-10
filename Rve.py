a82cfe875b790cd506aa910589118382bee17942
Daniel Enrique Caballero <danielcaballero88@gmail.com>
2019-05-10 12:50:53 -0300

    Outline (classes) defined

---
+++ Rve.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+class Nodo(object):
+    def __init__(self):
+        pass 
+
+class Segmento(object):
+    def __init__(self):
+        pass 
+
+class Fibra(object):
+    def __init__(self):
+        pass 
+
+class Layer(object):
+    def __init__(self):
+        pass 
+
+class Rve(object):
+    def __init__(self):
+        pass 
+
