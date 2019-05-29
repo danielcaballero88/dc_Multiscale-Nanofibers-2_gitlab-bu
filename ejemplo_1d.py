330310001afb47221f3070f9427c859fc89e87ec
Daniel Enrique Caballero <danielcaballero88@gmail.com>
2019-05-29 14:43:14 -0300

    Limpieza

--- ejemplo_1d.py
+++ ejemplo_1d.py
@@ -1,3 +1,20 @@
+"""
+Este ejemplo es una simulacion de un caso de 1d resuelto aparte
+son dos barras de longitudes l1=5 y l2=7.
+La barra 1 esta anclada en x=0, en x=5 están conectadas (nodo) 
+y el extremo de x=12 se lleva hasta x=20
+Entonces se encuentra un nuevo punto de equilibrio
+El resultado es completamente acorde a la simulacion 1d hecha aparte
+y tambien concuerda con los calculos manuales de equilibrio para este caso sencillo
+.
+Quizas preocupa un poco que sean 43 las iteraciones para obtener el equilibrio
+Pero la tolerancia esta puesta bastante baja y la pseudoviscosidad quizas muy alta
+(de hecho con una pseudoviscosidad*0.1 son 15 iteraciones (+2 inestables))
+(con pseudoviscosidad*0.01 son 10 iteraciones (+5 previas inestables), notar que esta es adaptiva)
+(con *0.001 son 6 iteraciones (+8 previas inestables))
+tal parece que empezar con poca pseudoviscosidad es buen plan
+"""
+
 from Malla import Nodos, Subfibras, Malla, Iterador
 import numpy as np
 
@@ -39,6 +56,7 @@ print "---"
 pseudovis = [2.0, 2.0, 2.0]
 
 m = Malla(n, s, pseudovis)
+#m.psv = m.psv*0.1
 
 F = np.array(
     [
